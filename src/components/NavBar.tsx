092aec7afd46384c370d7642fd7d9a8b9bec6c16
Alice A <alice.anglesjo@hotmail.com>
2024-08-27 11:04:26 +0200

    fix: trigger

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,12 @@
-import React, { FC } from "react";
-import { Button } from "./ui/button";
-import { Bell } from "lucide-react";
+import React, { FC } from 'react';
+import { Button } from './ui/button';
+import { Bell } from 'lucide-react';
 
 export const NavBar: FC = () => {
   return (
     <header>
       <nav className="flex flex-row justify-between px-4 py-6 items-center">
-        <h2>When do you get on?</h2>
+        <h2>Title</h2>
         <Button size="icon" className="rounded-full">
           <Bell size={22} />
         </Button>
