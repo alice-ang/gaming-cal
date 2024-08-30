4d6e4a88c2c61a6aa2a15f47df0ffb40d228b5aa
Alice A <alice.anglesjo@hotmail.com>
2024-08-30 17:34:35 +0200

    more clean up

--- src/app/calendar/[id]/page.tsx
+++ src/app/calendar/[id]/page.tsx
@@ -1,6 +1,5 @@
 import { CalendarEdit, ScheduleCalendar, ShareCalendar } from '@/components';
 import { Badge } from '@/components/ui/badge';
-import { getCalendarById } from '@/lib/data/actions';
 const bookedDays = [
   new Date(2024, 7, 8),
   new Date(2024, 7, 9),
@@ -12,13 +11,12 @@ export default async function CalendarPage({
 }: {
   params: { id: string };
 }) {
-  const calendar = await getCalendarById(params.id);
   return (
     <main className="p-4 constraints space-y-6 lg:space-y-6">
       {/* <LoginForm /> */}
 
       {/* <CalendarEdit /> */}
-      <h1 className="text-3xl font-bold mb-6">{calendar.title}</h1>
+      <h1 className="text-3xl font-bold mb-6">Tiele</h1>
       <div className="space-x-4">
         <Badge variant="success">All Available</Badge>
         <Badge variant="secondary">Your slots</Badge>
